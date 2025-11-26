b545273c4cd8e1aa403a10a6731ff9720ff3f34e
CHRAIBI Mohamed <chraibimohammed253@gmail.com>
2025-11-26 09:13:01 +0100

    Initial commit

---
+++ src/main/java/ma/Mundiapolis/CHRAIBImohamed/Thymeleaf/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package ma.Mundiapolis.CHRAIBImohamed.Thymeleaf.repository; // Dossier où se trouve le repository
+
+import ma.Mundiapolis.CHRAIBImohamed.Thymeleaf.entities.Product; // J'utilise l'entité Product
+import org.springframework.data.jpa.repository.JpaRepository; // Fournit toutes les opérations CRUD + JPA avancées
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Interface qui gère l'accès à la table Product dans la base de données
+    // JpaRepository donne automatiquement : findAll(), save(), deleteById(), findById(), etc.
+    // Product = type de l'entité, Long = type de l'id
+}
